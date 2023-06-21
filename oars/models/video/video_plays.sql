dbc9db9055f8f4ff89b942c937e6d9fa567e72d0
Rory Sawyer <rory@sawyer.dev>
2023-06-21 15:27:16 -0400

    feat: adds models for video engagement reports
    `video_plays` contains a record for each time a learner played a video.
    Can be used to generate reports of total plays and unique watchers
    
    `transcript_usage` contains a record for each time a transcript or
    closed caption was enabled. Can be used to generate reports for total
    transcription/caption usage as well as the number of unique learners who
    enabled transcription/closed captioning

---
+++ oars/models/video/video_plays.sql
@@ -0,0 +1,13 @@
+-- model to support number of watches per video
+-- ref: https://edx.readthedocs.io/projects/edx-insights/en/latest/Reference.html#engagement-computations
+
+select
+    emission_time,
+    org,
+    course_id,
+    splitByString('/xblock/', object_id)[2] as video_id,
+    actor_id
+from
+    {{ source('xapi', 'video_playback_events') }}
+where
+    event_type = 'play'
