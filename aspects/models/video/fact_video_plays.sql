2c2ee46079c37399f3c111ccdec2cdc4f8f6c40b
Rory Sawyer <rory@sawyer.dev>
2023-08-02 14:12:18 -0400

    feat: move video models into dbt
    This change replaces the existing `video_plays` and `transcript_usage`
    models with the queries for the `fact_video_plays` and
    `fact_transcript_usage` virtual datasets in Superset. This change would
    enable those datasets to become physical datasets that pull directly
    from these models

---
+++ aspects/models/video/fact_video_plays.sql
@@ -0,0 +1,28 @@
+-- model to support number of watches per video
+-- ref: https://edx.readthedocs.io/projects/edx-insights/en/latest/Reference.html#engagement-computations
+with plays as (
+    select
+        emission_time,
+        org,
+        course_key,
+        splitByString('/xblock/', object_id)[-1] as video_id,
+        actor_id
+    from
+        {{ source('xapi', 'video_playback_events') }}
+    where
+        verb_id = 'https://w3id.org/xapi/video/verbs/played'
+)
+
+select
+    plays.emission_time as emission_time,
+    plays.org as org,
+    courses.course_name as course_name,
+    splitByString('+', courses.course_key)[-1] as run_name,
+    blocks.block_name as video_name,
+    plays.actor_id as actor_id
+from
+    plays
+    join {{ source('event_sink', 'course_names')}} courses
+         on plays.course_key = courses.course_key
+    join {{ source('event_sink', 'course_block_names')}} blocks
+         on plays.video_id = blocks.location
