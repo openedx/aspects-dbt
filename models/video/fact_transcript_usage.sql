47857ad284249db75ab7afdaa6e483df110b01c7
Rory Sawyer <rory@sawyer.dev>
2023-12-18 12:17:49 -0500

    feat: add MV for transcript usage
    This update creates a new materialized view for transcript
    enabled/disabled events and updates `fact_transcript_usage` to use this
    new MV. This should improve the performance of transcript-related charts
    in Superset by avoiding relying on the base `xapi_events_all_parsed` table.

--- models/video/fact_transcript_usage.sql
+++ models/video/fact_transcript_usage.sql
@@ -1,17 +1,3 @@
-with transcripts as (
-    select
-        emission_time,
-        org,
-        splitByString('/', course_id)[-1] as course_key,
-        splitByString('/xblock/', object_id)[2] as video_id,
-        actor_id
-    from
-        {{ ref('xapi_events_all_parsed') }}
-    where
-        verb_id = 'http://adlnet.gov/expapi/verbs/interacted'
-        and JSON_VALUE(event_str, '$.result.extensions."https://w3id.org/xapi/video/extensions/cc-enabled"') = 'true'
-)
-
 select
     transcripts.emission_time as emission_time,
     transcripts.org as org,
@@ -23,7 +9,9 @@ select
     blocks.display_name_with_location as video_name_with_location,
     transcripts.actor_id as actor_id
 from
-    transcripts
+    {{ ref('video_transcript_events') }} transcripts
     join {{ ref('dim_course_blocks')}} blocks
          on (transcripts.course_key = blocks.course_key
              and transcripts.video_id = blocks.block_id)
+where
+    transcripts.cc_enabled
