8d6b3f57c86a7a8d47ffa5a622edb96757930c37
Cristhian Garcia <crisgarta8@gmail.com>
2024-02-19 10:34:13 -0500

    fix: include '@' symbol in problem id regex

--- macros/get_problem_id.sql
+++ macros/get_problem_id.sql
@@ -3,5 +3,5 @@
 -- either the problem id is at the end of the object ID
 -- or is followed by '/answer' or '/hint'
 {% macro get_problem_id(object_id) %}
-   regexpExtract(object_id, 'xblock/([\w\d-\+:]*@problem\+block@[\w\d][^_]*)(_\d_\d)?', 1)
+   regexpExtract(object_id, 'xblock/([\w\d-\+:@]*@problem\+block@[\w\d][^_]*)(_\d_\d)?', 1)
 {% endmacro %}
