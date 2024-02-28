42ab6222d48cf1574fe324638642187e728782ae
Cristhian Garcia <crisgarta8@gmail.com>
2024-02-28 10:46:15 -0500

    chore: remove latest_grade_status CTE

--- models/grading/fact_grade_status.sql
+++ models/grading/fact_grade_status.sql
@@ -1,21 +1,8 @@
 with
-    latest_grade_status as (
-        select
-            org, course_key, object_id, actor_id, max(emission_time) as last_grade_time
-        from {{ ref("grading_events") }}
-        where
-            verb_id in (
-                'http://adlnet.gov/expapi/verbs/passed',
-                'http://adlnet.gov/expapi/verbs/failed'
-            )
-        group by org, course_key, object_id, actor_id
-    ),
-
     latest_grades as (
         select
             org,
             course_key,
-            object_id,
             actor_id,
             splitByString('/', verb_id)[-1] as latest_state,
             row_number() over (
@@ -45,17 +32,10 @@ with
 select
     lg.org as org,
     lg.course_key as course_key,
-    lg.object_id as object_id,
     lg.actor_id as actor_id,
     lg.latest_state as latest_state,
     lc.course_grade as course_grade
 from latest_grades lg
-join
-    latest_grade_status lgs
-    on lg.org = lgs.org
-    and lg.course_key = lgs.course_key
-    and lg.object_id = lgs.object_id
-    and lg.actor_id = lgs.actor_id
 left join
     latest_course_grades lc
     on lg.org = lc.org
