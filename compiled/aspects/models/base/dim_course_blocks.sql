9a0020a987261328770cd52ca34d7e71324f81be
Ian2012 <Ian2012@users.noreply.github.com>
2024-03-05 14:32:02 +0000

    docs: update docs for c569a2291059e9423bc38cfac2c61757d35acfdb

--- compiled/aspects/models/base/dim_course_blocks.sql
+++ compiled/aspects/models/base/dim_course_blocks.sql
@@ -5,7 +5,17 @@ select
     courses.course_run as course_run,
     blocks.location as block_id,
     blocks.block_name as block_name,
-    blocks.display_name_with_location as display_name_with_location
+    blocks.display_name_with_location as display_name_with_location,
+    graded,
+    case
+        when block_id like '%@chapter+block@%'
+        then 'section'
+        when block_id like '%@sequential+block@%'
+        then 'subsection'
+        when block_id like '%@vertical+block@%'
+        then 'unit'
+        else regexpExtract(block_id, '@([^+]+)\+block@', 1)
+    end as block_type
 from `event_sink`.`course_block_names` blocks
 join
     `event_sink`.`course_names` courses
