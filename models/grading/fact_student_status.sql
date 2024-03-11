2b493e0dc6d3f391c2db24c370ba05ec21ef7ce8
Cristhian Garcia <crisgarta8@gmail.com>
2024-03-11 16:10:44 -0500

    feat: implement fact_student_status model

--- models/grading/fact_student_status.sql
+++ models/grading/fact_student_status.sql
@@ -5,7 +5,8 @@ select
     courses.course_name as course_name,
     courses.course_run as course_run,
     coalesce(state, 'failed') as state,
-    emission_time,
+    enrollment_status,
+    lg.emission_time,
     course_grade as course_grade,
     {{ get_bucket("course_grade") }} as grade_bucket
 from {{ ref("fact_learner_course_grade") }} ls
@@ -14,6 +15,11 @@ left join
     on ls.org = lg.org
     and ls.course_key = lg.course_key
     and ls.actor_id = lg.actor_id
+left join
+    {{ ref("fact_enrollment_status") }} fes
+    on ls.org = fes.org
+    and ls.course_key = fes.course_key
+    and ls.actor_id = fes.actor_id
 join
     {{ source("event_sink", "course_names") }} courses
     on ls.course_key = courses.course_key
