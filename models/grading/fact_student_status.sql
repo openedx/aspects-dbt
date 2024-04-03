5ed21365225ab27f8cf6e0d29972e823487b53f8
Cristhian Garcia <crisgarta8@gmail.com>
2024-04-03 16:17:38 -0500

    fix: add course_run to fact_satus models

--- models/grading/fact_student_status.sql
+++ models/grading/fact_student_status.sql
@@ -1,29 +1,29 @@
 select
-    ls.org as org,
-    ls.course_key as course_key,
-    ls.course_run as course_run,
-    ls.actor_id as actor_id,
+    fes.org as org,
+    fes.course_key as course_key,
+    fes.course_run as course_run,
+    fes.actor_id as actor_id,
     courses.course_name as course_name,
     coalesce(approving_state, 'failed') as approving_state,
     enrollment_mode,
     enrollment_status,
     course_grade as course_grade,
     {{ get_bucket("course_grade") }} as grade_bucket
-from {{ ref("fact_learner_course_grade") }} ls
+from {{ ref("fact_enrollment_status") }} fes
 left join
     {{ ref("fact_learner_course_status") }} lg
-    on ls.org = lg.org
-    and ls.course_key = lg.course_key
-    and ls.course_run = lg.course_run
-    and ls.actor_id = lg.actor_id
+    on fes.org = lg.org
+    and fes.course_key = lg.course_key
+    and fes.course_run = lg.course_run
+    and fes.actor_id = lg.actor_id
 left join
-    {{ ref("fact_enrollment_status") }} fes
-    on ls.org = fes.org
-    and ls.course_key = fes.course_key
-    and ls.course_run = fes.course_run
-    and ls.actor_id = fes.actor_id
+    {{ ref("fact_learner_course_grade") }} ls
+    on fes.org = fes.org
+    and fes.course_key = ls.course_key
+    and fes.course_run = ls.course_run
+    and fes.actor_id = ls.actor_id
 join
     {{ source("event_sink", "course_names") }} courses
-    on ls.org = courses.org
-    and ls.course_key = courses.course_key
-    and ls.course_run = courses.course_run
+    on fes.org = courses.org
+    and fes.course_key = courses.course_key
+    and fes.course_run = courses.course_run
