1130261a40092c193d0845dfe85561f8d5374eb5
Ian2012 <Ian2012@users.noreply.github.com>
2024-04-04 18:03:56 +0000

    docs: update docs for 2b97bde95a731538ef9be0cef6077e77b34400c3

---
+++ run/aspects/models/video/fact_video_engagement.sql
@@ -0,0 +1,64 @@
+
+
+  create view `xapi`.`fact_video_engagement` 
+  
+    
+    
+  as (
+    with
+    viewed_subsection_videos as (
+        select distinct
+            date(emission_time) as viewed_on,
+            org,
+            course_key,
+            
+    concat(
+        splitByString(
+            ':', splitByString(' - ', video_name_with_location)[1], 1
+        )[1],
+        ':0:0'
+    )
+ as section_number,
+            
+    concat(
+        arrayStringConcat(
+            splitByString(
+                ':', splitByString(' - ', video_name_with_location)[1], 2
+            ),
+            ':'
+        ),
+        ':0'
+    )
+
+            as subsection_number,
+            graded,
+            actor_id,
+            video_id
+        from `xapi`.`fact_video_plays`
+    )
+
+select
+    views.viewed_on,
+    views.org,
+    views.course_key,
+    videos.section_with_name,
+    videos.subsection_with_name,
+    videos.item_count,
+    views.actor_id,
+    views.video_id,
+    views.graded
+from viewed_subsection_videos views
+join
+    `xapi`.`int_videos_per_subsection` videos
+    on (
+        views.org = videos.org
+        and views.course_key = videos.course_key
+        and views.section_number = videos.section_number
+        and views.subsection_number = videos.subsection_number
+    )
+  )
+      
+      
+                    -- end_of_sql
+                    
+                    
--- run/aspects/models/video/fact_video_plays.sql
+++ run/aspects/models/video/fact_video_plays.sql
@@ -31,6 +31,7 @@ select
     plays.video_id as video_id,
     blocks.block_name as video_name,
     blocks.display_name_with_location as video_name_with_location,
+    blocks.graded as graded,
     video_position,
     video_duration,
     case
