299a5d74b69b43ed42a7f0d0e29250512cca7927
rory sawyer <rory@sawyer.dev>
2024-04-09 16:26:56 -0400

    feat: include anchor tag for problem, video links
    Adding links to problems and videos from the course dashboard will
    provide useful context when looking at charts of problem responses and
    video views.

---
+++ tests/a_tag.sql
@@ -0,0 +1,7 @@
+select
+    'http://local.edly.io/xblock/block-v1:OpenedX+DemoX+DemoCourse+type@video+block@d5a54ce52f464acfa7a83ae155712cc3' as object_id,
+    'The Purpose, Power and Reach of the Open edX® Platform' as video_name,
+    {{ a_tag('object_id', 'video_name') }} as object_a_tag
+from
+    system.numbers
+where object_a_tag != '<a href="http://local.edly.io/xblock/block-v1:OpenedX+DemoX+DemoCourse+type@video+block@d5a54ce52f464acfa7a83ae155712cc3" target="_blank">The Purpose, Power and Reach of the Open edX® Platform</a>'
