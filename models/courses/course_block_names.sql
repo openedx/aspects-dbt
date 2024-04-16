674d1d8a6afb75f231118513feefec7e0af25847
Cristhian Garcia <crisgarta8@gmail.com>
2024-04-16 11:26:05 -0500

    chore: remove most_recent_course_blocks

--- models/courses/course_block_names.sql
+++ models/courses/course_block_names.sql
@@ -16,6 +16,31 @@
     )
 }}
 
+with
+    most_recent_course_blocks as (
+        select
+            location,
+            display_name,
+            toString(section)
+            || ':'
+            || toString(subsection)
+            || ':'
+            || toString(unit)
+            || ' - '
+            || display_name as display_name_with_location,
+            JSONExtractInt(xblock_data_json, 'section') as section,
+            JSONExtractInt(xblock_data_json, 'subsection') as subsection,
+            JSONExtractInt(xblock_data_json, 'unit') as unit,
+            JSONExtractBool(xblock_data_json, 'graded') as graded,
+            course_key,
+            dump_id,
+            time_last_dumped,
+            row_number() over (
+                partition by location order by time_last_dumped desc
+            ) as rn
+        from {{ source("event_sink", "course_blocks") }}
+    )
 select
     location, display_name as block_name, course_key, graded, display_name_with_location
-from {{ ref("most_recent_course_blocks") }}
+from most_recent_course_blocks
+where rn = 1
