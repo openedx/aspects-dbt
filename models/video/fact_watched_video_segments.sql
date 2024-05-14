46c3d226b15977b4687120f2390ed25b0750cc1c
Cristhian Garcia <crisgarta8@gmail.com>
2024-05-14 17:00:04 -0500

    tmp

--- models/video/fact_watched_video_segments.sql
+++ models/video/fact_watched_video_segments.sql
@@ -1,54 +1,8 @@
 with
-    video_events as (
-        select
-            emission_time,
-            org,
-            course_key,
-            video_id,
-            actor_id,
-            verb_id,
-            video_position,
-            video_duration
-        from {{ ref("video_playback_events") }}
-    ),
-    starts as (
-        select *
-        from video_events
-        where verb_id = 'https://w3id.org/xapi/video/verbs/played'
-    ),
-    ends as (
-        select *
-        from video_events
-        where
-            verb_id in (
-                'http://adlnet.gov/expapi/verbs/completed',
-                'https://w3id.org/xapi/video/verbs/seeked',
-                'https://w3id.org/xapi/video/verbs/paused',
-                'http://adlnet.gov/expapi/verbs/terminated'
-            )
-    ),
     segments as (
         select
-            starts.org as org,
-            starts.course_key as course_key,
-            starts.video_id as video_id,
-            starts.actor_id,
-            cast(starts.video_position as Int32) as start_position,
-            cast(ends.video_position as Int32) as end_position,
-            starts.emission_time as started_at,
-            ends.emission_time as ended_at,
-            ends.verb_id as end_type,
-            starts.video_duration as video_duration
-        from starts left
-        asof join
-            ends
-            on (
-                starts.org = ends.org
-                and starts.course_key = ends.course_key
-                and starts.video_id = ends.video_id
-                and starts.actor_id = ends.actor_id
-                and starts.emission_time < ends.emission_time
-            )
+            *
+        from {{ ref("watched_video_segments") }}
     ),
     enriched_segments as (
         select
