be273861e68e21b2b22b2ea1a1cf3f12f2b5f682
Cristhian Garcia <crisgarta8@gmail.com>
2024-05-14 16:45:13 -0500

    tmp

--- models/video/video_playback_events.sql
+++ models/video/video_playback_events.sql
@@ -3,7 +3,7 @@
         materialized="materialized_view",
         schema=env_var("ASPECTS_XAPI_DATABASE", "xapi"),
         engine=get_engine("ReplacingMergeTree()"),
-        primary_key="(org, course_key, verb_id)",
+        primary_key="(org, course_key, video_id)",
         order_by="(org, course_key, verb_id, emission_time, actor_id, video_position, event_id)",
         partition_by="(toYYYYMM(emission_time))",
         ttl=env_var("ASPECTS_DATA_TTL_EXPRESSION", ""),
