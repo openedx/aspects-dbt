668f81cec9bd950970eee91d7e13790d646ae7dd
Cristhian Garcia <crisgarta8@gmail.com>
2024-05-27 11:24:04 -0500

    feat: move page engagement to an mv
    fix: use macro on int_pages_per_subsection

--- models/navigation/fact_pageview_engagement.sql
+++ models/navigation/fact_pageview_engagement.sql
@@ -1,97 +1,50 @@
 with
-    subsection_counts as (
+    subsection_engagement as (
         select
             org,
             course_key,
-            course_run,
-            section_with_name,
-            subsection_with_name,
-            actor_id,
-            page_count,
-            count(distinct block_id) as pages_visited,
-            case
-                when pages_visited = 0
-                then 'No pages viewed yet'
-                when pages_visited = page_count
-                then 'All pages viewed'
-                else 'At least one page viewed'
-            end as engagement_level
-        from {{ ref("fact_navigation_completion") }}
-        group by
-            org,
-            course_key,
-            course_run,
-            section_with_name,
-            subsection_with_name,
-            actor_id,
-            page_count
-    ),
-    section_counts as (
-        select
-            org,
-            course_key,
-            course_run,
-            section_with_name,
-            '' as subsection_with_name,
-            actor_id,
-            sum(page_count) as page_count,
-            sum(pages_visited) as pages_visited,
-            case
-                when pages_visited = 0
-                then 'No pages viewed yet'
-                when pages_visited = page_count
-                then 'All pages viewed'
-                else 'At least one page viewed'
-            end as engagement_level
-        from subsection_counts
-        group by
-            org,
-            course_key,
-            course_run,
-            section_with_name,
-            subsection_with_name,
-            actor_id
-    ),
-    pageview_counts as (
-        select
-            org,
-            course_key,
-            course_run,
-            section_with_name as section_with_name,
-            subsection_with_name as subsection_with_name,
-            subsection_with_name as section_subsection_name,
             'subsection' as content_level,
-            actor_id as actor_id,
+            actor_id,
+            subsection_block_id as block_id,
             engagement_level as section_subsection_page_engagement
-        from subsection_counts
-        union all
+        from {{ ref("subsection_page_engagement") }}
+    ),
+    section_engagement as (
         select
             org,
             course_key,
-            course_run,
-            section_with_name as section_with_name,
-            subsection_with_name as subsection_with_name,
-            section_with_name as section_subsection_name,
             'section' as content_level,
-            actor_id as actor_id,
+            actor_id,
+            section_block_id as block_id,
             engagement_level as section_subsection_page_engagement
-        from section_counts
-
+        from {{ ref("section_page_engagement") }}
+    ),
+    page_engagement as (
+        select *
+        from subsection_engagement
+        union all
+        select *
+        from section_engagement
     )
 
 select
     pv.org as org,
     pv.course_key as course_key,
-    pv.course_run as course_run,
-    pv.section_with_name as section_with_name,
-    pv.subsection_with_name as subsection_with_name,
-    pv.section_subsection_name as section_subsection_name,
+    course_blocks.course_run as course_run,
+    course_blocks.display_name_with_location as section_subsection_name,
     pv.content_level as content_level,
     pv.actor_id as actor_id,
     pv.section_subsection_page_engagement as section_subsection_page_engagement,
     users.username as username,
     users.name as name,
     users.email as email
-from pageview_counts pv
+from page_engagement pv
+join
+    {{ ref("dim_course_blocks") }} course_blocks
+    on (
+        pv.org = course_blocks.org
+        and pv.course_key = course_blocks.course_key
+        and pv.block_id = course_blocks.block_id
+    )
 left outer join
-    {{ ref("dim_user_pii") }} users on toUUID(actor_id) = users.external_user_id
+    {{ ref("dim_user_pii") }} users on toUUID(pv.actor_id) = users.external_user_id
