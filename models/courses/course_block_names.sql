d16858afabbb322ce9d4f10408cf12a3055422f7
Brian Mesick <bmesick@tcril.org>
2024-06-05 10:30:29 -0400

    refactor: Create new section/subsection block names dictionaries
    Previously finding section/subsection was tying up a lot of resources, this attempts to make those values faster and easier to locate while still keeping them fresh.

--- models/courses/course_block_names.sql
+++ models/courses/course_block_names.sql
@@ -4,11 +4,14 @@
         schema=env_var("ASPECTS_EVENT_SINK_DATABASE", "event_sink"),
         fields=[
             ("location", "String"),
-            ("block_name", "String"),
             ("course_key", "String"),
-            ("graded", "Bool"),
-            ("course_order", "Int32"),
+            ("block_name", "String"),
             ("display_name_with_location", "String"),
+            ("section_id", "Int32"),
+            ("subsection_id", "Int32"),
+            ("unit_id", "Int32"),
+            ("course_order", "Int32"),
+            ("graded", "Bool"),
         ],
         primary_key="location",
         layout="COMPLEX_KEY_SPARSE_HASHED()",
@@ -23,22 +26,20 @@
 with
     most_recent_course_blocks as (
         select
-            location,
-            display_name,
-            toString(section)
+            location as location,
+            course_key as course_key,
+            display_name as block_name,
+            toString(section_id)
             || ':'
-            || toString(subsection)
+            || toString(subsection_id)
             || ':'
-            || toString(unit)
-            || ' - '
-            || display_name as display_name_with_location,
-            JSONExtractInt(xblock_data_json, 'section') as section,
-            JSONExtractInt(xblock_data_json, 'subsection') as subsection,
-            JSONExtractInt(xblock_data_json, 'unit') as unit,
-            JSONExtractBool(xblock_data_json, 'graded') as graded,
+            || toString(unit_id) as hierarchy_location,
+            hierarchy_location || ' - ' || block_name as display_name_with_location,
+            JSONExtractInt(xblock_data_json, 'section') as section_id,
+            JSONExtractInt(xblock_data_json, 'subsection') as subsection_id,
+            JSONExtractInt(xblock_data_json, 'unit') as unit_id,
             `order` as course_order,
-            course_key,
-            dump_id,
+            JSONExtractBool(xblock_data_json, 'graded') as graded,
             time_last_dumped,
             row_number() over (
                 partition by location order by time_last_dumped desc
@@ -46,11 +47,14 @@ with
         from {{ source("event_sink", "course_blocks") }}
     )
 select
-    location,
-    display_name as block_name,
-    course_key,
-    graded,
-    course_order,
-    display_name_with_location
+    location as location,
+    course_key as course_key,
+    block_name as block_name,
+    display_name_with_location as display_name_with_location,
+    section_id as section_id,
+    subsection_id as subsection_id,
+    unit_id as unit_id,
+    course_order as course_order,
+    graded as graded
 from most_recent_course_blocks
 where rn = 1
--- models/courses/dim_course_blocks.sql
+++ models/courses/dim_course_blocks.sql
@@ -5,13 +5,16 @@ select
     courses.course_run as course_run,
     blocks.location as block_id,
     blocks.block_name as block_name,
-    {{ section_from_display("blocks.display_name_with_location") }} as section_number,
-    {{ subsection_from_display("blocks.display_name_with_location") }}
-    as subsection_number,
-    splitByString(' - ', blocks.display_name_with_location)[1] as hierarchy_location,
+    blocks.section_id as section_id,
+    blocks.subsection_id as subsection_id,
+    blocks.unit_id as unit_id,
+    sections.location as section_block_id,
+    sections.section_name_with_location as section_name_with_location,
+    subsections.location as subsection_block_id,
+    subsections.subsection_name_with_location as subsection_name_with_location,
     blocks.display_name_with_location as display_name_with_location,
-    course_order,
-    graded,
+    course_order as course_order,
+    graded as graded,
     case
         when block_id like '%@chapter+block@%'
         then 'section'
@@ -24,4 +27,10 @@ select
 from {{ ref("course_block_names") }} blocks
 join
     {{ ref("course_names") }} courses on blocks.course_key = courses.course_key
-    settings join_algorithm = 'direct'
+join
+    {{ ref("section_block_names") }} sections on blocks.course_key = sections.course_key
+        and blocks.section_id = sections.section_id
+join
+    {{ ref("subsection_block_names") }} subsections on blocks.course_key = subsections.course_key
+    and blocks.section_id = subsections.section_id
+    and blocks.subsection_id = subsections.subsection_id
