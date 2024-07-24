fdd20cf80c8219f3fceb18ffc46bad09c51b4972
Cristhian Garcia <crisgarta8@gmail.com>
2024-07-24 12:08:39 -0500

    feat: migrate recent blocks mv to dbt

--- models/courses/course_block_names.sql
+++ models/courses/course_block_names.sql
@@ -21,24 +21,10 @@
 }}
 
 select
-    course_blocks.location as location,
+    location as location,
     display_name as block_name,
     course_key,
-    JSONExtractBool(xblock_data_json, 'graded') as graded,
+    graded,
     order as course_order,
-    toString(JSONExtractInt(xblock_data_json, 'section'))
-    || ':'
-    || toString(JSONExtractInt(xblock_data_json, 'subsection'))
-    || ':'
-    || toString(JSONExtractInt(xblock_data_json, 'unit'))
-    || ' - '
-    || display_name as display_name_with_location
-from {{ source("event_sink", "course_blocks") }} course_blocks
-join
-    (
-        select location, max(time_last_dumped) as max_time_last_dumped
-        from {{ source("event_sink", "course_blocks") }}
-        group by location
-    ) latest_course_blocks
-    on course_blocks.location = latest_course_blocks.location
-    and course_blocks.time_last_dumped = latest_course_blocks.max_time_last_dumped
+    display_name_with_location
+from {{ ref("most_recent_course_blocks") }} course_blocks
