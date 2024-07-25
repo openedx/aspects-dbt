8d890f25cc791d46f54ce73a37cbb8d99a9dae04
Brian Mesick <bmesick@tcril.org>
2024-07-25 11:06:37 -0400

    refactor: Reduce memory usage on dict queries

--- models/courses/course_block_names.sql
+++ models/courses/course_block_names.sql
@@ -20,37 +20,25 @@
     )
 }}
 
-with
-    most_recent_course_blocks as (
-        select
-            location,
-            display_name,
-            toString(section)
-            || ':'
-            || toString(subsection)
-            || ':'
-            || toString(unit)
-            || ' - '
-            || display_name as display_name_with_location,
-            JSONExtractInt(xblock_data_json, 'section') as section,
-            JSONExtractInt(xblock_data_json, 'subsection') as subsection,
-            JSONExtractInt(xblock_data_json, 'unit') as unit,
-            JSONExtractBool(xblock_data_json, 'graded') as graded,
-            `order` as course_order,
-            course_key,
-            dump_id,
-            time_last_dumped,
-            row_number() over (
-                partition by location order by time_last_dumped desc
-            ) as rn
-        from {{ source("event_sink", "course_blocks") }}
-    )
 select
-    location,
+    course_blocks.location as location,
     display_name as block_name,
     course_key,
-    graded,
-    course_order,
-    display_name_with_location
-from most_recent_course_blocks
-where rn = 1
+    JSONExtractBool(xblock_data_json, 'graded') as graded,
+    order as course_order,
+    toString(JSONExtractInt(xblock_data_json, 'section'))
+    || ':'
+    || toString(JSONExtractInt(xblock_data_json, 'subsection'))
+    || ':'
+    || toString(JSONExtractInt(xblock_data_json, 'unit'))
+    || ' - '
+    || display_name as display_name_with_location
+from {{ source("event_sink", "course_blocks") }} course_blocks
+join
+    (
+        select location, max(time_last_dumped) as max_time_last_dumped
+        from {{ source("event_sink", "course_blocks") }}
+        group by location
+    ) latest_course_blocks
+    on course_blocks.location = latest_course_blocks.location
+    and course_blocks.time_last_dumped = latest_course_blocks.max_time_last_dumped
