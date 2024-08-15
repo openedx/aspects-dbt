391186deb1bd1429401a01e8a665edb8ce96969a
Sara Burns <sburns@axim.org>
2024-08-15 16:36:25 -0400

    test: tests for macros, fixed broken unit test

---
+++ tests/macros/subsection_from_display.sql
@@ -0,0 +1,33 @@
+select 
+    '1:2:3 - Chapter 123' as display_name_with_location
+from system.one
+where 
+    {{ subsection_from_display('display_name_with_location') }}
+    != '1:2:0'
+
+union all 
+
+select 
+    '1:2:33 - Problem 456' as display_name_with_location
+from system.one
+where 
+    {{ subsection_from_display('display_name_with_location') }}
+    != '1:2:0'
+
+union all 
+
+select 
+    '1:22:3 - Sequential 789' as display_name_with_location
+from system.one
+where 
+    {{ subsection_from_display('display_name_with_location') }}
+    != '1:22:0'
+
+union all 
+
+select 
+    '11:2:3 - Vertical 123' as display_name_with_location
+from system.one
+where 
+    {{ subsection_from_display('display_name_with_location') }}
+    != '11:2:0'
