e0fba35339b879d5f4b124b33ace37289b71dcd1
Sara Burns <sburns@axim.org>
2025-05-21 15:08:14 -0400

    wip

--- models/video/fact_video_engagement.sql
+++ models/video/fact_video_engagement.sql
@@ -1,67 +1,50 @@
-{{
-    config(
-        materialized="materialized_view",
-        engine=get_engine("ReplacingMergeTree()"),
-        order_by="(org, course_key, actor_id)",
-    )
-}}
-
 with
-    video_engagment as (
+    subsection_engagement as (
         select
             org,
             course_key,
+            'subsection' as content_level,
             actor_id,
-            case
-                when videos_viewed = 0
-                then 'No videos viewed yet'
-                when videos_viewed = item_count
-                then 'All videos viewed'
-                else 'At least one video viewed'
-            end as section_subsection_video_engagement,
-            block_id,
-            section_subsection_name,
-            content_level
-        from {{ ref("fact_video_section_subsection") }} ARRAY
-        join
-            arrayConcat([subsection_block_id], [section_block_id]) as block_id,
-            arrayConcat(
-                [subsection_with_name], [section_with_name]
-            ) as section_subsection_name,
-            arrayConcat(
-                [subsection_content_level], [section_content_level]
-            ) as content_level
-        group by
-            org,
-            course_key,
-            actor_id,
-            section_subsection_video_engagement,
-            block_id,
-            section_subsection_name,
-            content_level
+            subsection_block_id as block_id,
+            engagement_level as section_subsection_video_engagement
+        from {{ ref("fact_subsection_video_engagement") }}
     ),
-    final_results as (
+    section_engagement as (
         select
-            video_engagment.org as org,
-            video_engagment.course_key as course_key,
-            video_engagment.section_subsection_name as section_subsection_name,
-            video_engagment.content_level as content_level,
-            video_engagment.actor_id as actor_id,
-            video_engagment.section_subsection_video_engagement
-            as section_subsection_video_engagement,
-            users.username as username,
-            users.name as name,
-            users.email as email
-        from video_engagment
-        left outer join
-            {{ ref("dim_user_pii") }} users
-            on (
-                video_engagment.actor_id like 'mailto:%'
-                and SUBSTRING(video_engagment.actor_id, 8) = users.email
-            )
-            or video_engagment.actor_id = toString(users.external_user_id)
-        where section_subsection_name <> ''
+            org,
+            course_key,
+            'section' as content_level,
+            actor_id,
+            section_block_id as block_id,
+            engagement_level as section_subsection_video_engagement
+        from {{ ref("fact_section_video_engagement") }}
+    ),
+    video_engagement as (
+        select *
+        from subsection_engagement
+        union all
+        select *
+        from section_engagement
     )
-select *
-from final_results
-
+select
+    ve.org as org,
+    ve.course_key as course_key,
+    course_blocks.display_name_with_location as section_subsection_name,
+    ve.content_level as content_level,
+    ve.actor_id as actor_id,
+    ve.section_subsection_video_engagement as section_subsection_video_engagement,
+    users.username as username,
+    users.name as name,
+    users.email as email
+from video_engagement ve
+join
+    {{ ref("dim_course_blocks") }} course_blocks
+    on (
+        ve.org = course_blocks.org
+        and ve.course_key = course_blocks.course_key
+        and ve.block_id = course_blocks.block_id
+    )
+left outer join
+    {{ ref("dim_user_pii") }} users
+    on (ve.actor_id like 'mailto:%' and SUBSTRING(ve.actor_id, 8) = users.email)
+    or ve.actor_id = toString(users.external_user_id)
