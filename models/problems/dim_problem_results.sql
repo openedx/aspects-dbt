5e0bc205902925dbcbcd481085ac3f0df3e736f2
Sara Burns <sburns@axim.org>
2025-06-06 11:02:18 -0400

    fix: Show 0% for problems with no correct responses

--- models/problems/dim_problem_results.sql
+++ models/problems/dim_problem_results.sql
@@ -10,25 +10,35 @@
 with
     final_results as (
         select
-            blocks.org as org,
-            blocks.course_key as course_key,
+            events.org as org,
+            events.course_key as course_key,
             first_success.success as success,
             first_success.attempt as attempt,
             first_success.actor_id as actor_id,
-            splitByChar('@', block_id)[3] as block_id_short,
+            splitByChar('@', blocks.block_id)[3] as block_id_short,
             {{
                 format_problem_number_location(
                     "first_success.object_id", "blocks.display_name_with_location"
                 )
             }}
-        from {{ ref("dim_course_blocks") }} as blocks
+        from
+            (
+                select distinct org, course_key, object_id, problem_id
+                from {{ ref("problem_events") }}
+            ) events
+        join
+            {{ ref("dim_course_blocks") }} as blocks
+            on (
+                events.course_key = blocks.course_key
+                and events.problem_id = blocks.block_id
+            )
         left join
             {{ ref("dim_learner_first_success_response") }} as first_success
             on (
-                first_success.course_key = blocks.course_key
-                and first_success.problem_id = blocks.block_id
+                first_success.org = events.org
+                and first_success.course_key = events.course_key
+                and first_success.object_id = events.object_id
             )
-        where blocks.block_type = 'problem'
     )
 select
     org,
